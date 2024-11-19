30407fa39dba198c9ce63dd4ca2a750779bf2af1
Ruslan Valeev <ruvaleev@gmail.com>
2024-11-19 18:10:20 +0700

    Add PROMETHEUS_EXPORTER_HOST usage to prometheus_exporter

--- config/initializers/prometheus_exporter.rb
+++ config/initializers/prometheus_exporter.rb
@@ -1,6 +1,10 @@
 unless Rails.env.test?
   require 'prometheus_exporter/middleware'
 
+  PrometheusExporter::Client.default = PrometheusExporter::Client.new(
+    host: ENV['PROMETHEUS_EXPORTER_HOST'] || 'localhost', port: 9394
+  )
+
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware
 end
